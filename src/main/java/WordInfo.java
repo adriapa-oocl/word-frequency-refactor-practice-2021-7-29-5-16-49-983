e28b7192f8852b16020d3801065b20eff167d5dc
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-09 15:32:35 +0800

    [WordFrequency][ADRIAPA]refactor: renamed the Input class to WordInfo and its parameters to word and count

---
+++ src/main/java/WordInfo.java
@@ -0,0 +1,20 @@
+public class WordInfo {
+    private String word;
+    private int count;
+
+    public WordInfo(String word, int count){
+        this.word =word;
+        this.count =count;
+    }
+
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public int getWordCount() {
+        return this.count;
+    }
+
+
+}
